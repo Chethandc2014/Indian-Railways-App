a5c11b2d97fe66ca7597d798c73959d13de7f775
ADMIN <Chethandc2014@gmail.com>
2017-11-08 11:35:35 +0530

    adding module in memory api to application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+
+
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
+import { InMemeoryLoginService } from './mock-services/in-memeory-login-service';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -12,12 +17,14 @@ import { HttpModule } from '@angular/http';
   declarations: [
     AppComponent,
     HomepageComponent,
-    LoginComponent
+    LoginComponent ,
+    
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemeoryLoginService)
   ],
   providers: [],
   bootstrap: [AppComponent]
