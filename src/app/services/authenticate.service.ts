94a9a95fdbc08924a095ba97f5227fd15b579aba
ADMIN <Chethandc2014@gmail.com>
2017-11-08 11:32:46 +0530

    Modifying authentication service based on in memory web api

--- src/app/services/authenticate.service.ts
+++ src/app/services/authenticate.service.ts
@@ -1,16 +1,40 @@
-import { User } from '../models/user';
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { log } from 'util';
+import {User} from '../models/user';
+import {Injectable} from '@angular/core';
+import {Http, RequestOptions, RequestOptionsArgs, Headers} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import {log} from 'util';
+import 'rxjs/add/operator/toPromise';
+
 
 @Injectable()
 export class AuthenticateService {
 
-  constructor() { }
-  
-  login(loginUser:User):boolean{
-    log('Beging of login method UserDtails::'+loginUser);
-    return true;
+  constructor(private http: Http) {}
+
+  login(loginUser: User): Promise<any> {
+    log('Beging of login method UserDtails::' + loginUser);
+    let name=loginUser.name;
+    let password=loginUser.password;
+    return this.http.get('api/authentication?name=${name}&&password=${password}').toPromise().then(res => {
+      console.log(res.json().data);
+      //data=res.json().data.length>0;
+    }
+    );
   }
+
+  //Login Post Request
+  /*loginPost(loginUser: User): void {
+    let header = new Headers({'Content-Type': 'application/json'});
+    let option = new RequestOptions({headers: header});
+     this.http.post('api/authentication', {'user': loginUser}, option)
+      .catch(this.loginError).subscribe(response=>response.json().data);
+
+  }
+
+  loginError(error: any): Observable<Response> {
+    console.log('Error while fetchind data from Server...');
+    return;
+  }*/
 
 }
