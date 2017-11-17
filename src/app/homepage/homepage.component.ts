438b3663487a26b22627c4ed15190302a9b94eea
Chethan D C <Chethandc2014@gmail.com>
2017-11-18 00:55:38 +0530

    Adding tabs to the search Result page

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -11,10 +11,30 @@ export class HomepageComponent implements OnInit {
   constructor() { }
 
   bsConfig:Partial<BsDatepickerConfig>;
+  tabs:any[]=[];
   ngOnInit() {
    this.bsConfig=Object.assign({},{containerClass:'theme-default'});
   }
   public dateOfJourney:Date=new Date();
+
+  addNewTab(): void {
+    const newTabIndex = this.tabs.length + 1;
+    this.tabs.push({
+      title: `${newTabIndex}`,
+      content: `Dynamic content ${newTabIndex}`,
+      removable: true
+    });
+  };
+
+  
+  removeTabHandler(tab: any): void {
+    this.tabs.splice(this.tabs.indexOf(tab), 1);
+   // console.log('Remove Tab handler');
+  }
+
+
+
+
   
    
 }
